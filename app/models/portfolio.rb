9be9f81d6b6d725979b74b914def0c242c3e17eb
Jeremiah Claspell <coloradobowhunter@live.com>
2018-05-13 11:21:38 -0600

    implimented default values

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Angular')
   end
   scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||=  "http://via.placeholder.com/400x600"  
+    self.thumb_image||= "https://media1.tenor.com/images/a322c03a94505b92e5e284e03ff0e3be/tenor.gif?itemid=5892980"
+  end
 end
